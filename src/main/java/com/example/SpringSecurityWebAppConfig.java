ad7675ca9b739acb1e4f48ed90fd0d76761ec468
Raghav SaKe <raghavsake@gmail.com>
2020-12-14 06:05:19 +0530

    initial-commit

---
+++ src/main/java/com/example/SpringSecurityWebAppConfig.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import static com.stormpath.spring.config.StormpathWebSecurityConfigurer.stormpath;
+
+
+@Configuration
+public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.apply(stormpath()).and()
+                .authorizeRequests()
+                .antMatchers("/").permitAll();
+    }
+}
